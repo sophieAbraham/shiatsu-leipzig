231a6f37a2bf317219f4f951f963b284fb73f809
Sophie Abraham <sophie.abraham@posteo.de>
2023-05-24 22:52:43 +0200

    + modified sliderData.js

--- src/components/layout/sliderData.js
+++ src/components/layout/sliderData.js
@@ -1,22 +1,22 @@
 export const sliderData = [
     {
         id: 0,
-        src: './IMG_20220501_092303 (2).jpg',
+        src: '../assets/img/IMG_20220501_092303 (2).jpg',
         figcaption: 'Buch: Yu Sen Sprudelnder Quell, Autor: Wilfried Rappenecker, Illustrationen: Richard Hartwell',
     },
     {
         id: 1,
-        src: './DSC03652-300.jpg',
+        src: '../assets/img/DSC03652-300.jpg',
         figcaption: 'Behandlung Blasen-Meridian',
     },
     {
         id: 2,
-        src: "./DSC03649-300.jpg",
+        src: "../assets/img/DSC03649-300.jpg",
         figcaption: 'Behandlung Nieren-Meridian',
     },
     {
         id: 3,
-        src: "./IMG_20220520_110105-300.jpg",
+        src: "../assets/img/IMG_20220520_110105-300.jpg",
         figcaption: 'Hara-Diagnose',
     }
 ];
